90162a8255332328679fdfca057e6d0b24dca608
haval95 <40795444+halenkurdi@users.noreply.github.com>
2021-03-21 21:09:40 +0300

    Feat: setup language button
    closes: #23

--- src/i18n.js
+++ src/i18n.js
@@ -1,28 +1,27 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import en from "./locales/en.json";
-import kr from "./locales/kr.json";
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import en from './locales/en.json'
+import kr from './locales/kr.json'
 
 const resources = {
-  en: {
-    translation: en,
-  },
-  
-  kr: {
-    translation: kr,
-  },
-};
-
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources,
-    fallbackLng: "en",
-    debug: true,
-    interpolation: {
-      escapeValue: false,
+    en: {
+        translation: en,
     },
-  });
-export default i18n;
+
+    kr: {
+        translation: kr,
+    },
+}
+
+i18n.use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        fallbackLng: 'en',
+        debug: true,
+        interpolation: {
+            escapeValue: false,
+        },
+    })
+export default i18n
