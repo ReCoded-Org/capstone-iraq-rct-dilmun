f259e9fb3914080e7398c935aa86fcabb332bad0
unknown <ismail.th.khalil@gmail.com>
2021-03-22 19:27:53 +0300

    add files for cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
+import SearchPageCard from './components/SearchPageCard/SearchPageCard'
 import Home from './containers/Home/Home'
 
 function App() {
     return (
         <div className="App">
             <Home />
+            <SearchPageCard />
         </div>
     )
 }
---
+++ src/components/SearchPageCard/SearchPageCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SearchPageCard() {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
