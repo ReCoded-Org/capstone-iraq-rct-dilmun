c58b56c1a6f024063495020ea42ee4d12b1e49fd
Muhanned Noman <Muhanned.Noman@gmail.com>
2021-03-23 20:11:54 +0300

    Fix: update Profile folder name

---
+++ src/containers/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+import './Profile.css'
+import ImageInfo from '../../components/navIInfo'
+
+export default function Profile() {
+    return (
+        <>
+            <ImageInfo />
+        </>
+    )
+}
