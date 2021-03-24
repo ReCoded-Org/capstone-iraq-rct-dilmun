69dd8f9246f754ec700dfe83f67b819ef58152d9
haval95 <40795444+halenkurdi@users.noreply.github.com>
2021-03-24 14:13:42 +0300

    Feat: contact info card and font awsome icons
    here I have created contact info card that is shown in the product detail page, and also initializaed fontawsome

---
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function ProductDetail() {
+    return (
+        <div>
+            <b className="block m-1">Title Here</b>
+            <p className="bg-yellow p-1 m-1 text-black inline-block">
+                Price: $123
+            </p>
+            <b className="block m-1 mt-3"> Description</b>
+            <p className=" m-1 text-black ">
+                Description here Description here Description here Description
+                here Description here Description here Description here
+                Description here Description here Description here Description
+                here Description here Description here Description here
+                Description here Description here Description here Description
+                here Description hereDescription here Description here
+                Description here Description here Description here Description
+                here Description here Description here Description here
+                Description here
+            </p>
+        </div>
+    )
+}
