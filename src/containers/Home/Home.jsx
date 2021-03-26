251c414c9d6e86e407bdb427accbe7b45d4431e9
unknown <ismail.th.khalil@gmail.com>
2021-03-26 19:04:54 +0300

    Feat: Landing Page card
    Created Landing Page Card
    
    closes #49

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import LandingPageCard from '../../components/LandingPageCard'
 
 export default function Home() {
     return (
         <div>
             Home Page
-            
+            <LandingPageCard />
         </div>
     )
 }
