accf27088995b31b964bd2d246fa22719d36bc42
haval95 <40795444+halenkurdi@users.noreply.github.com>
2021-03-26 12:14:30 +0300

    Refactor: props for the item detail component

--- src/containers/ProductDetails/ProductDetails.jsx
+++ src/containers/ProductDetails/ProductDetails.jsx
@@ -7,7 +7,11 @@ export default function ProductDetails() {
         <div className="  lg:space-x-5 grid  grid-cols-1 lg:grid-cols-3 lg:mx-20  ">
             <div className=" w-full md:col-span-2 my-20 ">
                 <SlideShow />
-                <ItemDetail />
+                <ItemDetail
+                    title="title"
+                    price="price"
+                    description="description"
+                />
             </div>
 
             <div className="lg:w-10/12 w-full  my-20 justify-self-center">
