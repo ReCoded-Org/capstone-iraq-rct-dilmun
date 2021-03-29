8df66abe7551c5c8586a6180e670455682df9ab5
Muhanned Noman <Muhanned.Noman@gmail.com>
2021-03-29 19:29:30 +0300

    Fix: setup husky pre commit hook
    This will allow the code to be formated and chekced befoer commiting

--- src/App.js
+++ src/App.js
@@ -12,36 +12,32 @@ import SearchResult from './containers/SearchResult/SearchResult'
 import AddItem from './containers/AddItem/AddItem'
 import './fontawsome'
 import {
-    HOME_ROUTE,
-    CONTACT_ROUTE,
-    ABOUT_ROUTE,
-    PRODUCT_DETAILS_ROUTE,
-    SEARCH_RESULT_ROUTE,
-    PROFILE_ROUTE,
-    ADD_ITEM_ROUTE,
+  HOME_ROUTE,
+  CONTACT_ROUTE,
+  ABOUT_ROUTE,
+  PRODUCT_DETAILS_ROUTE,
+  SEARCH_RESULT_ROUTE,
+  PROFILE_ROUTE,
+  ADD_ITEM_ROUTE,
 } from './router'
 
 function App() {
-    return (
-        <>
-            <NavBar />
+  return (
+    <>
+      <NavBar />
 
-            <Switch>
-                <Route exact path={HOME_ROUTE} component={Home} />
-                <Route path={PROFILE_ROUTE} component={Profile} />
-                <Route path={ABOUT_ROUTE} component={About} />
-                <Route path={CONTACT_ROUTE} component={Contact} />
-                <Route
-                    path={PRODUCT_DETAILS_ROUTE}
-                    component={ProductDetails}
-                />
-                <Route path={SEARCH_RESULT_ROUTE} component={SearchResult} />
-                <Route path={ADD_ITEM_ROUTE} component={AddItem} />
-            </Switch>
-            <Footer/>
-
-        </>
-    )
+      <Switch>
+        <Route exact path={HOME_ROUTE} component={Home} />
+        <Route path={PROFILE_ROUTE} component={Profile} />
+        <Route path={ABOUT_ROUTE} component={About} />
+        <Route path={CONTACT_ROUTE} component={Contact} />
+        <Route path={PRODUCT_DETAILS_ROUTE} component={ProductDetails} />
+        <Route path={SEARCH_RESULT_ROUTE} component={SearchResult} />
+        <Route path={ADD_ITEM_ROUTE} component={AddItem} />
+      </Switch>
+      <Footer />
+    </>
+  )
 }
 
 export default App
