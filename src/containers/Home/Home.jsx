005e7071a9a54e662618ee33af28cb4b79b31ead
haval95 <40795444+halenkurdi@users.noreply.github.com>
2021-04-03 01:08:57 +0300

    Feat: home hero header
    closes: #118

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from '../../components/Header'
 import ItemsCarousal from '../../components/ItemsCarousal'
 
 export default function Home() {
   return (
     <div>
-      Home Page
+      <Header />
       <h1 className="font-bold p-5 text-xl"> Donated Items</h1>
       <ItemsCarousal />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            black: '#0boa07',
-            white: '#f7f7f9',
-            darkBlue: '#0050b3',
-            blue: '#4a99d3',
-            lightBlue: '#d4e4f1',
-            red: '#F26C75',
-            yellow: '#f3d30e',
-            orange: '#e67a00',
-            green: '#7fe98a',
-            darkgray: '#B4BEC8',
-            grey: '#E5E5E5',
-        },
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  theme: {
+    colors: {
+      pureWhite: '#FFFFFF',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#0boa07',
+      white: '#f7f7f9',
+      darkBlue: '#0050b3',
+      blue: '#4a99d3',
+      lightBlue: '#d4e4f1',
+      red: '#F26C75',
+      yellow: '#f3d30e',
+      orange: '#e67a00',
+      green: '#7fe98a',
+      darkgray: '#B4BEC8',
+      grey: '#E5E5E5',
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
