c64966264f85b2767a73f66a85663e151dbbe61e
Haval Rezan <76211550+haval95@users.noreply.github.com>
2021-04-02 16:11:43 +0300

    Style: Navbar (#112)
    Style: Navbar
    
    z-index fixed, changed the style to sticky top, changed active links to bold
    
    closes:#111

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -38,7 +38,7 @@ export default function Navbra() {
   }
   return (
     <>
-      <nav className="bg-white">
+      <nav className="bg-white z-50 sticky top-0  ">
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
           <div className="relative flex items-center justify-between h-16">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -103,28 +103,28 @@ export default function Navbra() {
                     className=" hover:text-darkBlue px-3 py-2 rounded-md text-sm font-medium"
                     exact
                     to={HOME_ROUTE}
-                    activeClassName="text-darkBlue"
+                    activeClassName="text-darkBlue font-bold"
                   >
                     {t('navbar.home')}
                   </NavLink>
                   <NavLink
                     className=" hover:text-darkBlue px-3 py-2 rounded-md text-sm font-medium"
                     to={CONTACT_ROUTE}
-                    activeClassName="text-darkBlue"
+                    activeClassName="text-darkBlue font-bold"
                   >
                     {t('navbar.contact')}
                   </NavLink>
                   <NavLink
                     className=" hover:text-darkBlue px-3 py-2 rounded-md text-sm font-medium"
                     to={ABOUT_ROUTE}
-                    activeClassName="text-darkBlue"
+                    activeClassName="text-darkBlue font-bold"
                   >
                     {t('navbar.about')}
                   </NavLink>
                   <NavLink
                     className=" hover:text-darkBlue px-3 py-2 rounded-md text-sm font-medium"
                     to={PROFILE_ROUTE}
-                    activeClassName="text-darkBlue"
+                    activeClassName="text-darkBlue font-bold"
                   >
                     {t('footer.profile')}
                   </NavLink>
@@ -168,18 +168,18 @@ export default function Navbra() {
                 <div
                   className={`${
                     profileDropDown ? ' show' : ' hidden'
-                  } origin-top-right absolute right-0 mt-2 w-48 rounded-md items-center shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none`}
+                  } origin-top-right absolute right-0 border border-t-0 boder-grey mt-2 w-48 rounded-md items-center shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none`}
                   role="menu"
                   aria-orientation="vertical"
                   aria-labelledby="user-menu"
                 >
-                  <a
-                    href="/"
+                  <NavLink
+                    to={PROFILE_ROUTE}
                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-darkBlue hover:text-white"
                     role="menuitem"
                   >
                     {user.isLoggedIn ? user.user.name : ''}
-                  </a>
+                  </NavLink>
                   <NavLink
                     to="/"
                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-darkBlue hover:text-white "
@@ -206,28 +206,28 @@ export default function Navbra() {
           id="mobile-menu"
         >
           <div className="px-2 pt-2 pb-3 space-y-1 text-blue">
-            <a
-              href="/"
+            <NavLink
+              to={HOME_ROUTE}
               className="text-darkBlue block px-3 py-2 rounded-md text-base font-medium"
             >
               Home
-            </a>
-            <a
-              href="/"
+            </NavLink>
+            <NavLink
+              to={CONTACT_ROUTE}
               className="text-blue  hover:text-darkBlue block px-3 py-2 rounded-md text-base font-medium"
             >
               Contact
-            </a>
-            <a
-              href="/"
+            </NavLink>
+            <NavLink
+              to={ABOUT_ROUTE}
               className="text-blue hover:text-darkBlue block px-3 py-2 rounded-md text-base font-medium"
             >
               About
-            </a>
+            </NavLink>
             <NavLink
-              to="/profile"
+              to={PROFILE_ROUTE}
               className="hover:text-darkBlue block px-3 py-2 rounded-md text-base font-medium"
-              activeClassName="text-darkBlue"
+              activeClassName="text-darkBlue font-bold"
             >
               Profile
             </NavLink>
