080145d193c5cf161268b5f0db48e5c1b07f950d
Haval Rezan <76211550+haval95@users.noreply.github.com>
2021-04-03 14:12:32 +0300

    Revert "style: navbar shadow"

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -36,7 +36,7 @@ export default function Navbra() {
   }
   return (
     <>
-      <nav className="bg-white z-50 sticky top-0 shadow-md ">
+      <nav className="bg-white z-50 sticky top-0  ">
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
           <div className="relative flex items-center justify-between h-16">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
