46cb6822b70a012ee3a67d3ed6868cfb9a174004
unknown <ismail.th.khalil@gmail.com>
2021-04-04 00:09:52 +0300

    fixed the small screen

--- src/containers/SearchResult/SearchResult.jsx
+++ src/containers/SearchResult/SearchResult.jsx
@@ -3,7 +3,7 @@ import FilterOptions from '../../components/FilterOptions/FilterOptions'
 
 export default function SearchResult() {
   return (
-    <div className="grid sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 p-3">
+    <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 p-3">
       <div className="grid grid-col-1">
         <FilterOptions />
       </div>
